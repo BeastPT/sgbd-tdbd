4b465cce097c4da4b5e290599f784d37e473d190
beastpt <ajfa2004@gmail.com>
2023-11-15 13:48:52 +0000

    Primeira tabela + Estrutura Formulario, Gestao de Registos

--- php/gestao-de-registos.php
+++ php/gestao-de-registos.php
@@ -12,7 +12,7 @@ $queryChilds = "SELECT * from child ORDER BY name ASC";
 $childs = mysqli_query($sql, $queryChilds);
 if (mysqli_num_rows($childs) == 0) {
     echo "
-        <h3>Não há crianças</h3>
+        <h3>Não existem crianças</h3>
     ";
 } else {
     echo "
@@ -32,7 +32,7 @@ if (mysqli_num_rows($childs) == 0) {
         $queryItems = "SELECT DISTINCT i.name AS name, i.id as id FROM item AS i, subitem AS si, value AS v WHERE v.subitem_id = si.id AND i.id = si.item_id AND v.child_id = ".$child["id"]." ORDER BY subitem_id ASC";
         $items = mysqli_query($sql, $queryItems);
         while ($item = mysqli_fetch_assoc($items)) {
-            echo "<br>".strtoupper($item["name"]).":<br>";
+            echo "<u>".strtoupper($item["name"]).":</u><br>";
             $queryEdits = "
             SELECT DISTINCT v.date AS date, v.producer as author
             FROM value AS v, subitem AS si WHERE
@@ -43,13 +43,13 @@ if (mysqli_num_rows($childs) == 0) {
             ";
             $edits = mysqli_query($sql, $queryEdits);
             while ($edit = mysqli_fetch_assoc($edits)) {
-                echo "<br>[editar][apagar] - <b>".$edit["date"]."</b> (".$edit["author"].") -";
+                echo "[editar][apagar] - <strong>".$edit["date"]."</strong> (".$edit["author"].") -";
 
                 $queryValues = "
-                SELECT v.date AS date, v.producer AS author, si.name AS name, v.value AS value, si_type.name AS type
+                SELECT si.name AS name, v.value AS value, si_type.name AS type
                 FROM subitem AS si, value AS v, subitem_unit_type AS si_type
                 WHERE
-                    v.date = ".$edit["date"]." AND
+                    v.date = '".$edit["date"]."' AND
                     si.item_id = ".$item["id"]." AND
                     si_type.id = si.unit_type_id AND
                     si.id = v.subitem_id AND
@@ -58,11 +58,9 @@ if (mysqli_num_rows($childs) == 0) {
                 ";
                 $values = mysqli_query($sql, $queryValues);
                 while ($value = mysqli_fetch_assoc($values)) {
-                    r($value);
-                    //echo "
-                    //<b>".$value["name"]."</b> (".$value["value"]." ".$value["type"]."); 
-                    //";
+                    echo " <strong>".$value["name"]."</strong> (".$value["value"]." ".$value["type"].");";
                 }
+                echo "<br>";
             }
         }
         echo "
@@ -75,5 +73,23 @@ if (mysqli_num_rows($childs) == 0) {
         </table>
     ";
 }
+echo "<h3>Dados de registo - introdução</h3>";
+echo "<br><p>* Campos obrigatórios<p>";
+
+echo '
+    <form>
+        <label for="fullname_child">Nome completo: </label>*<br>
+        <input id ="fullname_child" type="text" name="fullname_child"><br>
+        <label for="birthdate">Data de Nascimento (AAAA-MM-DD): </label>*<br>
+        <input id ="birthdate" type="text" name="birthdate" placeholder="AAAA-MM-DD"><br>
+        <label for="fullname_tutor">Nome completo do encarregado de educação: </label>*<br>
+        <input id ="fullname_tutor" type="text" name="fullname_tutor"><br>
+        <label for="cellphone">Telefone do encarregado de educação (9 dígitos): </label>*<br>
+        <input id ="cellphone" type="number" name="cellphone" size="9"><br>
+        <label for="email">Endereço de e-mail do tutor: </label><br>
+        <input id ="email" type="email" name="email" placeholder="example@mail.com"><br>
+        <input type="submit" value="Submeter">
+    </form>
+';
 
 ?>
