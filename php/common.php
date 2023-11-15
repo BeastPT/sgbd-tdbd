ae94c9f9f754a89b9bbdd1ae83d2afab0f0ffcf8
beastpt <ajfa2004@gmail.com>
2023-11-15 13:52:40 +0000

    common.php - add VerifyCapability; add global

--- php/common.php
+++ php/common.php
@@ -1,5 +1,10 @@
 <?php
+global $current_page;
+$current_page = get_site_url().'/'.basename(get_permalink());
 
+function verifyCapability($capability) {
+    return current_user_can($capability) && is_user_logged_in();
+}
 
 function connectDB() {
     $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
