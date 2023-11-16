21b625d61772a994c43cfa9f00d474545462e40a
beastpt <ajfa2004@gmail.com>
2023-11-16 15:47:12 +0000

    common.php - add test_input; add goBack

--- php/common.php
+++ php/common.php
@@ -2,6 +2,20 @@
 global $current_page;
 $current_page = get_site_url().'/'.basename(get_permalink());
 
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+function goBack(){
+    echo "<script type='text/javascript'>document.write(\"<a href='javascript:history.back()' class='backLink' title='Voltar atr&aacute;s'>Voltar atr&aacute;s</a>\");</script>
+    <noscript>
+    <a href='".$_SERVER['HTTP_REFERER']."‘ class='backLink' title='Voltar atr&aacute;s'>Voltar atr&aacute;s</a>
+    </noscript>";
+}
+
 function verifyCapability($capability) {
     return current_user_can($capability) && is_user_logged_in();
 }
