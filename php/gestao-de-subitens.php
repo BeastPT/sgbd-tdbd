7d571d881573fcfe3a71e3145ae222e5770a6684
Hugo Branco <hugo-b-11@hotmail.com>
2023-11-22 21:55:58 +0000

    fix subitem table and add rest of form

--- php/gestao-de-subitens.php
+++ php/gestao-de-subitens.php
@@ -2,6 +2,8 @@
 
 //Requer as funções presentes no common.php
 require_once("custom/php/common.php");
+//Requer o css
+require_once("custom/css/ag.css");
 
 $link = connectDB();
 
@@ -59,10 +61,10 @@ if (!verifyCapability('manage_subitems')) {
                 $rowspan_subitens = mysqli_num_rows($subitens);
 
                 if ($rowspan_subitens == 0) {
-
+                    //Se não houver subitens, é apresentada uma mensagem a dizer que não há subitens
                     echo "<tr>
                             <td>" . $item_rows['name'] . "</td>
-                            <td colspan = 10> Não há subitens</td>";
+                            <td colspan = 10>este item não tem subitens</td>";
                 } else {
 
                     echo "<tr>
@@ -72,39 +74,104 @@ if (!verifyCapability('manage_subitems')) {
 
                 while ($subitens_rows = mysqli_fetch_assoc($subitens)) {
 
-                    //criação das rows que vão conter a informação com rowspan para os itens
-                    //Se não houver subitens, é apresentada uma mensagem a dizer que não há subitens
+                    //query das unidades
+                    $query_das_unidades = "SELECT name FROM subitem_unit_type WHERE subitem_unit_type.id = " . $subitens_rows['unit_type_id'] . "";
+                    $unidades = mysqli_query($link, $query_das_unidades);
 
+                    //criação das rows que vão conter a informação com rowspan para os itens
                     echo "  
                         <td>" . $subitens_rows['id'] . "</td>
                         <td>" . $subitens_rows['name'] . "</td>
                         <td>" . $subitens_rows['value_type'] . "</td>
                         <td>" . $subitens_rows['form_field_name'] . "</td>
-                        <td>" . $subitens_rows['form_field_type'] . "</td>
-                        <td>" . $subitens_rows['unit_type_id'] . "</td>
-                        <td>" . $subitens_rows['form_field_order'] . "</td>
-                        <td>" . $subitens_rows['mandatory'] . "</td>
-                        <td>" . $subitens_rows['state'] . "</td>
-                        <td>[editar][apagar]</td>
+                        <td>" . $subitens_rows['form_field_type'] . "</td>";
+
+                    if (!empty($subitens_rows['unit_type_id'])) {
+                        while ($unidades_rows = mysqli_fetch_assoc($unidades)) {
+                            echo "<td>" . $unidades_rows['name'] . "</td>";
+                        }
+                    } elseif (empty($subitens_rows['unit_type_id'])) {
+                        echo "<td> - </td>";
+                    }
+                    echo "
+                        <td>" . $subitens_rows['form_field_order'] . "</td>";
+                    if ($subitens_rows['mandatory'] == 1) {
+                        echo "<td>Sim</td>";
+                    } elseif ($subitens_rows['mandatory'] == 0) {
+                        echo "<td>Não</td>";
+                    }
+                    if ($subitens_rows['state'] == 'active') {
+                        echo "<td>Ativo</td>";
+                    } else {
+                        echo "<td>Inativo</td>";
+                    }
+                    echo "<td>[editar][apagar]</td>
                     </tr>";
                 }
             }
             //conclusão da table
             echo "</table>";
 
-
             echo "<h3>Gestão de subitems - introdução</h3>";
+
+            //query dos nomes dos itens
+            $item_name_query = "SELECT name FROM item";
+            $item_name = mysqli_query($link, $item_name_query);
+
+            //query dos tipos de unidade
+            $query_tipo_de_unidade = "SELECT name FROM subitem_unit_type";
+            $tipo_de_unidade = mysqli_query($link, $query_tipo_de_unidade);
+
+            //query dos tipos de valor
+            $value_type = get_enum_values("subitem", "value_type");
+
+            //query dos tipos de campo no formulário
+            $form_field_type = get_enum_values("subitem", "form_field_type");
 
             echo "
             <form>
-                Nome do subitem: <input type='text' name=subitem></br>
-                Tipo de Valor: <input type='radio' name=value_type></br>
-                Item: <select name='item'>
-                <input type='hidden' name='estado' value='inserir'>
+                Nome do subitem: <input type='text' name=subitem_name></br>
+                Tipo de Valor:</br>";
+            foreach ($value_type as $value) {
+                echo "<input type='radio' name=value_type value=" . $value . ">" . $value . "</br>";
+            }
+            echo "</br>";
+            //Item
+            echo "Item:</br>";
+            echo "<select name='item'>";
+            while ($item_name_rows = mysqli_fetch_assoc($item_name)) {
+                echo "<option value=" . $item_name_rows['name'] . ">" . $item_name_rows['name'] . "</option>";
+            }
+            echo "</select></br>";
+            echo "</br>";
+            //Tipo de campo no formulário
+            echo "Tipo de campo no formulário:</br>";
+            foreach ($form_field_type as $form_field) {
+                echo "<input type='radio' name=form_field_type value=" . $form_field . ">" . $form_field . "</br>";
+            }
+            echo "</br>";
+            //Tipo de unidade
+            echo "Tipo de unidade:</br>";
+            echo "<select name='unit_type'>";
+            while ($tipo_de_unidade_rows = mysqli_fetch_assoc($tipo_de_unidade)) {
+                echo "<option value=" . $tipo_de_unidade_rows['name'] . ">" . $tipo_de_unidade_rows['name'] . "</option>";
+            }
+            echo "</select></br>";
+            echo "
+                Ordem do campo no formulário: <input type='text' name=form_field_order></br>
+                Obrigatório:</br>";
+            echo "<input type='radio' name=mandatory value=1>Sim</br>";
+            echo "<input type='radio' name=mandatory value=0>Não</br>";
+
+            echo "<input type='hidden' name='estado' value='inserir'>
                 <input type='submit'>
             </form>
             ";
         }
     } elseif ($_REQUEST['estado'] == 'inserir') {
+
+        //validação dos campos
+        if (empty($_REQUEST['subitem_name'])) {
+        }
     }
 }
